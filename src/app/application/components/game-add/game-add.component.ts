ab7e800fd9ae8f618ad4420217e676a3c2bca48e
sara kalai <sara.kal2004@gmail.com>
2024-11-04 10:54:45 +0100

    partie 1 et 2 application tp8 finie

--- src/app/application/components/game-add/game-add.component.ts
+++ src/app/application/components/game-add/game-add.component.ts
@@ -1,13 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Category } from '../../model/category';
+import { ControlContainer, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { GameService } from '../../services/game.service';
+import { Game } from '../../model/game';
+import { JsonPipe } from '@angular/common';
 
 
 @Component({
   selector: 'app-game-add',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './game-add.component.html',
   styleUrl: './game-add.component.css'
 })
-export class GameAddComponent  {
+export class GameAddComponent implements OnInit {
+
+
+category:String[]=Object.values(Category);
+gameService:GameService=inject(GameService);
+games:Game[]=[];
+game!:Game
+
+gameForm:FormGroup=new FormGroup({
+  id:new FormControl(1, {nonNullable:true}),
+  name:new FormControl("echec", {nonNullable:true}),
+  price:new FormControl(46.3, {nonNullable:true}),
+  madeIn:new FormControl('Tunisie', {nonNullable:true}),
+  category:new FormControl(Category.BoardGames, {nonNullable:true}),
+  isNew:new FormControl(true, {nonNullable:true}),
+})
+ngOnInit(): void {
+  this.gameService.getGames().subscribe(
+    data=>{this.games=data
+      console.log(data)}
+  )
+ 
+}
+
+onSubmit(){
+  
+// console.log(this.gameForm.value);
+// console.log(this.gameForm.get('name')?.value);
+// console.log(this.gameForm.controls['price'].value);
+// console.log(this.gameForm.value['category']);
+// console.log(this.gameForm.value.madeIn);
+
+
+console.log(this.games);
+this.gameForm.get('id')?.setValue(this.games.length+1);
+this.game=new Game(this.gameForm.get('id')?.value,this.gameForm.value.name,this.gameForm.value.price,this.gameForm.value.madeIn,this.gameForm.value.category,this.gameForm.value.isNew);
+this.gameService.addGame(this.game).subscribe(
+  data=>{this.gameService.getGames().subscribe(
+    data=>{this.games=data
+    console.log(data)}
+  )
+  }
+)
+console.log(this.games);
+}
+onResetForm(){
+  console.log(this.games);
+this.gameForm.reset();
+this.gameForm.get('id')?.setValue(this.games.length+1);
+}
 
 }
--- src/app/application/services/game.service.ts
+++ src/app/application/services/game.service.ts
@@ -1,8 +1,19 @@
-import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Game } from '../model/game';
+import { Observable } from 'rxjs';
+const url="http://localhost:3000/games";
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
+   private readonly http:HttpClient=inject(HttpClient);
+
+  getGames():Observable<Game[]>{
+    return this.http.get<Game[]>(url);
+  }
+  addGame(g:Game):Observable<Game>{
+    return this.http.post<Game>(url,g);
+  }
 
 }
