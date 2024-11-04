c9634370cff8aae4c79adc496fd887971b2955ad
sara kalai <sara.kal2004@gmail.com>
2024-11-04 11:08:27 +0100

    partie 3 application tp8 finie

--- src/app/application/components/game-add/game-add.component.ts
+++ src/app/application/components/game-add/game-add.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Category } from '../../model/category';
-import { ControlContainer, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ControlContainer, FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { GameService } from '../../services/game.service';
 import { Game } from '../../model/game';
 import { JsonPipe } from '@angular/common';
@@ -19,17 +19,31 @@ export class GameAddComponent implements OnInit {
 category:String[]=Object.values(Category);
 gameService:GameService=inject(GameService);
 games:Game[]=[];
-game!:Game
+game!:Game;
+gameForm!:FormGroup;
 
-gameForm:FormGroup=new FormGroup({
-  id:new FormControl(1, {nonNullable:true}),
-  name:new FormControl("echec", {nonNullable:true}),
-  price:new FormControl(46.3, {nonNullable:true}),
-  madeIn:new FormControl('Tunisie', {nonNullable:true}),
-  category:new FormControl(Category.BoardGames, {nonNullable:true}),
-  isNew:new FormControl(true, {nonNullable:true}),
-})
+// gameForm:FormGroup=new FormGroup({
+//   id:new FormControl(1, {nonNullable:true}),
+//   name:new FormControl("echec", {nonNullable:true}),
+//   price:new FormControl(46.3, {nonNullable:true}),
+//   madeIn:new FormControl('Tunisie', {nonNullable:true}),
+//   category:new FormControl(Category.BoardGames, {nonNullable:true}),
+//   isNew:new FormControl(true, {nonNullable:true}),
+// })
+
+formBuilder:FormBuilder=inject(FormBuilder);
 ngOnInit(): void {
+this.gameForm=this.formBuilder.nonNullable.group({
+  id:[1],
+  name:[''],
+  price:[0],
+  madeIn:['Tunisie'],
+  category:[Category.BoardGames],
+  isNew:[true]
+})
+this.gameForm.get('name')?.valueChanges.subscribe(
+  (value)=>console.log(value)
+)
   this.gameService.getGames().subscribe(
     data=>{this.games=data
       console.log(data)}
@@ -39,7 +53,7 @@ ngOnInit(): void {
 
 onSubmit(){
   
-// console.log(this.gameForm.value);
+console.log(this.gameForm.value);
 // console.log(this.gameForm.get('name')?.value);
 // console.log(this.gameForm.controls['price'].value);
 // console.log(this.gameForm.value['category']);
@@ -62,6 +76,8 @@ onResetForm(){
   console.log(this.games);
 this.gameForm.reset();
 this.gameForm.get('id')?.setValue(this.games.length+1);
+this.gameForm.get('madeIn')?.setValue("Autre");
+this.gameForm.get('category')?.setValue(Category.CardGames);
 }
 
 }
